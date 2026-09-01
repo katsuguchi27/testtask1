ff8442c3d13e9d0c59c8ea1ed7cafce3c4783a1e
katsuguchi27 <128844865+katsuguchi27@users.noreply.github.com>
2026-09-01 16:30:41 +0000

    Create the repository and configure the Gradle

---
+++ weather-app/app/build.gradle.kts
@@ -0,0 +1,21 @@
+// Determine for Gradle kotlin version and repositories for JVM
+plugins { 
+    kotlin("jvm") version "2.3.0"
+    application
+}
+repositories {
+    mavenCentral()
+}
+//Determine the dependencies for work
+dependencies {
+    //Retrofit and Jackson converters
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.11.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+    
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+}
+application {
+    mainClass.set("org.example.MainKt")
+}
